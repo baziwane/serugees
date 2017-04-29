aa8712abb84284a11d036c014fafe584631e3f33
Raymond B Matovu <raymond.baziwane@gmail.com>
2017-04-29 16:46:00 -0500

    related to type cast issue

--- src/serugees-api/Models/ILoanRepository.cs
+++ src/serugees-api/Models/ILoanRepository.cs
@@ -6,8 +6,8 @@ namespace serugees_api.Models
     {
         void Add(Loan item);
         IEnumerable<Loan> GetAll();
-        Loan Find(long key);
-        void Remove(long key);
+        Loan Find(int key);
+        void Remove(int key);
         void Update(Loan item);
     }
 }
